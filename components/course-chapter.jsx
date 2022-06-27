440ed64a639a88a63c8fc1f5a72f581201c3a3f8
DESKTOP-JLMBUDS\Dai <nnhhaadd@gmail.com>
2022-06-27 22:45:08 +0700

    add delete chapter & lecture

--- components/course-chapter.jsx
+++ components/course-chapter.jsx
@@ -1,26 +1,29 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faPen } from "@fortawesome/free-solid-svg-icons"
+import { faPen, faXmark } from "@fortawesome/free-solid-svg-icons"
 import { useRef, useState } from "react"
 import Link from "next/link"
 import AnimateHeight from 'react-animate-height';
 import Divider from "./divider";
 import { v4 as uuidv4 } from 'uuid';
 
-function LectureEntry({lecture, isAdmin}) {
+function LectureEntry({lecture, isAdmin, onDelete = (lecture)=>{}}) {
   const [titleEdit, setTitleEdit] = useState(false)
   const titleInput = useRef(null)
   const [title, setTitle] = useState(lecture.title)
-  async function editChapterTitle(evt) {
+  async function editLectureTitle(evt) {
     evt.preventDefault()
     setTitleEdit(!titleEdit)
   }
-
+  async function deleteLecture(evt){
+    evt.preventDefault()
+    onDelete(lecture)
+  }
   return (
     <Link href={`/lecture/${lecture._id}`}>
       <a className="px-2 m-px w-full flex flex-row">
         {
           isAdmin
-          ?<button onClick={editChapterTitle} className="flex-none mx-2">
+          ?<button onClick={editLectureTitle} className="flex-none mx-2">
             <FontAwesomeIcon
               icon={faPen}
               style={{fontSize: 15}}
@@ -33,12 +36,22 @@ function LectureEntry({lecture, isAdmin}) {
           ?<input onClick={(evt => evt.preventDefault())} onChange={(evt)=>setTitle(evt.target.value)} className="rounded outline-0 bg-slate-300 flex-auto text-left underline" ref={titleInput} value={title}></input>
           :<h2 className="flex-auto text-left underline">{title}</h2>
         }
+        {
+          isAdmin
+          ?<button onClick={deleteLecture} className="flex-none mx-2">
+            <FontAwesomeIcon
+              icon={faXmark}
+              style={{fontSize: 15}}
+              className="text-red-500 hover:text-red-700"
+            ></FontAwesomeIcon>
+          </button>
+          :<></>}
       </a>
     </Link>
   )
 }
 
-export default function CourseChapter({ chapter, isAdmin })
+export default function CourseChapter({ chapter, isAdmin, onChapterDelete=(chapter)=>{} })
 {
   const [titleEdit, setTitleEdit] = useState(false)
   const [expand, setExpand] = useState(false)
@@ -70,6 +83,19 @@ export default function CourseChapter({ chapter, isAdmin })
     setLectures([...lectures, dummyLecture])
   }
 
+  async function deleteChapter(evt) {
+    evt.preventDefault()
+    onChapterDelete(chapter)
+  }
+
+  function onSubChapterDelete(delChapter) {
+    setChapters(chapters.filter(chapter => chapter._id != delChapter._id))
+  }
+
+  function onSubLectureDelete(delLecture) {
+    setLectures(lectures.filter(lecture => lecture._id != delLecture._id))
+  }
+
   return (
     <>
     {
@@ -86,14 +112,23 @@ export default function CourseChapter({ chapter, isAdmin })
           </button>
           :<></>  
       }
-      <button className={isAdmin?"rounded-r bg-blue-100 p-2 m-l-px w-full flex flex-row":"rounded bg-blue-100 p-2 m-l-px w-full flex flex-row"} onClick={(evt)=> {if(!titleEdit)setExpand(!expand)}}>
-        {
-          titleEdit
-          ?<input onClick={(evt => evt.preventDefault())} onChange={(evt)=>setTitle(evt.target.value)} className="rounded outline-0 bg-blue-300 flex-auto text-slate-700 text-left font-bold" ref={titleInput} value={title}></input>
-          :<h2 className="flex-auto text-slate-700 text-left font-bold">{title}</h2>
-        }
-        
+      <button className={isAdmin?"bg-blue-100 p-2 m-l-px w-full flex flex-row":"rounded bg-blue-100 p-2 m-l-px w-full flex flex-row"} onClick={(evt)=> {if(!titleEdit)setExpand(!expand)}}>
+      {
+        titleEdit
+        ?<input onClick={(evt => evt.preventDefault())} onChange={(evt)=>setTitle(evt.target.value)} className="rounded outline-0 bg-blue-300 flex-auto text-slate-700 text-left font-bold" ref={titleInput} value={title}></input>
+        :<h2 className="flex-auto text-slate-700 text-left font-bold">{title}</h2>
+      }
       </button>
+      {
+        isAdmin
+        ?<button onClick={deleteChapter} className="flex-none px-2 bg-blue-100 rounded-r">
+          <FontAwesomeIcon
+            icon={faXmark}
+            style={{fontSize: 15}}
+            className="text-red-500 hover:text-red-700"
+          ></FontAwesomeIcon>
+        </button>
+        :<></>}
       
       </div>
       :<></>
@@ -126,7 +161,7 @@ export default function CourseChapter({ chapter, isAdmin })
           <ul className="flex flex-col">
             {
               chapters.map(chapter => <li key={chapter._id} className='flex-auto'>
-                <CourseChapter chapter={chapter} isAdmin={isAdmin}></CourseChapter>
+                <CourseChapter chapter={chapter} isAdmin={isAdmin} onChapterDelete={onSubChapterDelete}></CourseChapter>
               </li>)
             }
             {
