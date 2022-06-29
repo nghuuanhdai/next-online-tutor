36a645ac850c68f5f9dcee5f9153738b9048cc09
DESKTOP-JLMBUDS\Dai <nnhhaadd@gmail.com>
2022-06-29 08:57:34 +0700

    create profile on first login

--- pages/api/sessionLogin.js
+++ pages/api/sessionLogin.js
@@ -1,5 +1,6 @@
 import { serialize } from 'cookie';
 import { getAuth } from "firebase-admin/auth";
+import Profile from '../../models/profile';
 import { getAdminApp } from '../../utils/firebaseAdmin';
 
 export default async function handler(req, res) {
@@ -15,23 +16,32 @@ export default async function handler(req, res) {
 
   const expiresIn = 1000*60*60*24*5;
   const auth = getAuth(getAdminApp())
-
+  let email = null
   try {
-      const decodedIdToken = await auth.verifyIdToken(idToken);
-      if (new Date().getTime() / 1000 - decodedIdToken.auth_time > 5 * 60)
-      {
-          res.status(401).send('Recent sign in required!');
-          return;
-      }
+    const decodedIdToken = await auth.verifyIdToken(idToken);
+    email = decodedIdToken.email
+    if (new Date().getTime() / 1000 - decodedIdToken.auth_time > 5 * 60)
+    {
+        res.status(401).send('Recent sign in required!');
+        return;
+    }
   } catch (error) {
-      console.log(error);
-      res.status(500).send('SERVER ERROR!');
-      return;
+    console.log(error);
+    res.status(500).send('SERVER ERROR!');
+    return;
   }
 
   try {
       const sessionCookie = await auth.createSessionCookie(idToken, { expiresIn })
       const options =  { maxAge: expiresIn, httpOnly: true, secure: process.env.NODE_ENV == 'development'?false:true, path: '/'};
+      
+      let userProfile = await Profile.findOne({email: email})
+      if(!userProfile)
+      {
+        userProfile = new Profile()
+        userProfile.email = email
+        await userProfile.save()
+      }
       res.setHeader('Set-Cookie', serialize('session', sessionCookie, options));
       res.status(200)
       res.end(JSON.stringify({ status: 'success' }));
